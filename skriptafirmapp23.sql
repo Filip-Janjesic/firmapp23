0934a2d014242c3782d05044240708b5730ceee6
Filip Janješić <filip.janjesic@gmail.com>
2021-05-12 20:07:20 +0200

    Kreiranje baze

--- skriptafirmapp23.sql
+++ skriptafirmapp23.sql
@@ -1,2 +1,10 @@
 drop database if exists firmapp23;
 create database firmapp23;
+use firmapp23;
+
+create table projekt(
+sifra int not null primary key auto_increment,
+naziv varchar(50);
+cijena decimal(18,2)
+);
+
